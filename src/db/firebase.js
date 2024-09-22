d82a8a23c9cb404bec6802171465c9819f8c697e
Thiago <thiago_ssfr@hotmail.com>
2024-09-22 15:04:30 -0300

    adicionado variaveis de ambiente

--- src/db/firebase.js
+++ src/db/firebase.js
@@ -3,13 +3,16 @@ import 'firebase/firestore';
 import 'firebase/auth';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBjdbFAITFX7-gi5hCK8LRFtilxWvkDJTk",
-    authDomain: "pucpr-14855.firebaseapp.com",
-    projectId: "pucpr-14855",
-    storageBucket: "pucpr-14855.appspot.com",
-    messagingSenderId: "761295444397",
-    appId: "1:761295444397:web:53871b665de21c4ca96924"
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_ID
 };
+
+console.log('teste', firebaseConfig);
+
 
 if (!Firebase.apps.length)
     Firebase.initializeApp(firebaseConfig);
